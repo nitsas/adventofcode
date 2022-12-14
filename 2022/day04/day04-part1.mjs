ccfbe074d8f5ef47069a7ba79817fd0546b36c50
Chris Nitsas <nitsas.chris@gmail.com>
2022-12-15 00:43:41 +0200

    [2022-day04] Solve part1 using Range.prototype.covers()

---
+++ 2022/day04/day04-part1.mjs
@@ -0,0 +1,17 @@
+// Usage: cat input.txt | node dayXX.mjs
+
+import * as readline from 'node:readline/promises';
+import { stdin } from 'node:process';
+import Range from '../datastructs/range.mjs';
+
+const input = readline.createInterface({ input: stdin });
+
+let numCoveredRanges = 0;
+for await (const line of input) {
+  const [range1, range2] = line.split(',').map((rangeStr) => new Range(rangeStr));
+  if (range1.covers(range2) || range2.covers(range1)) {
+    numCoveredRanges += 1;
+  }
+}
+
+console.log(numCoveredRanges);
